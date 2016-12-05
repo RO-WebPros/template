473ee867b5c1faf4f184e5a2b8edf90571cf4604
Dragos Vlad <dragos.vlad95@gmail.com>
2016-12-05 18:03:11 +0200

    Setup the gulp work enviroment

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Instructions for setup:
       - Create template folder
       - git init
       - git remote add template https://github.com/DragosVlad/template.git
-      - git pull template
+      - git pull template master
     
     Work:
       - Make changes
@@ -17,7 +17,19 @@ Instructions for setup:
       - git commit -m "your message here"
       - git push template
       
+
   
   GULP
-  
+    
+    Commands for initalisation:
+      - npm init
+      - npm install gulp --save-dev                         (install gulp)
+      - npm install es6-promise --save-dev                  (polyfill needed for some tasks)
+      - npm install gulp-sass gulp-autoprefixer --save-dev  (sass and autoprefixer)
+      - npm install gulp-plumber gulp-util --save-dev       (better error handling)
+      - npm install gulp-concat --save-dev                  (concatenate js files)
+      - npm install gulp-jshint --save-dev                  (validate js files)
+      - npm install gulp-uglify --save-dev                  (uglify js files)
+      - npm install browser-sync --save-dev                 (browsersync for automatic refresh)
+      - create gulpfile.js
     
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+require('es6-promise').polyfill();
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
+var plumber       = require('gulp-plumber');
+var gutil         = require('gulp-util');
+var concat        = require('gulp-concat');
+var jshint        = require('gulp-jshint');
+var uglify        = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+
+var onError = function (err) {
+  console.log('An error occurred:', gutil.colors.magenta(err.message));
+  gutil.beep();
+  this.emit('end');
+};
+
+
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
+  .pipe(plumber({ errorHandler: onError }))
+  .pipe(sass())
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('./css'));
+});
+
+
+gulp.task('js', function() {
+  return gulp.src(['./js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('script.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./js'))
+});
+
+
+gulp.task('watch', function() {
+  browserSync.init({
+    files: ['./**/*.php', './**/*.html'],
+    proxy: 'http://localhost/template/',
+  });
+  gulp.watch('./sass/**/*.scss', ['sass', reload]);
+  gulp.watch('./js/*.js', ['js', reload]);
+});
+
+
+gulp.task('default', ['sass', 'js', 'watch']);
