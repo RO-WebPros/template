2ad49eb28ab0aac8d7459ac277da85ce5f7565cf
Valcea Daniel <daniel.valcea@gmail.com>
2016-12-06 14:37:29 +0200

    modificare gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -31,13 +31,8 @@ gulp.task('js', function() {
   return gulp.src(['./js/*.js'])
   .pipe(jshint())
     .pipe(jshint.reporter('default'))
-<<<<<<< HEAD
 //     .pipe(concat('script.js'))
 //     .pipe(uglify())
-=======
-    // .pipe(concat('script.js'))
-    // .pipe(uglify())
->>>>>>> b3bf3f735b71d336735417bffe8f16d9bd5a7b3b
     .pipe(gulp.dest('./js'))
 });
 
@@ -45,12 +40,8 @@ gulp.task('js', function() {
 gulp.task('watch', function() {
   browserSync.init({
     files: ['./**/*.php', './**/*.html'],
-<<<<<<< HEAD
-    proxy: 'http://localhost:81',
-    port:80,
-=======
-    proxy: 'http://localhost/template/',
->>>>>>> b3bf3f735b71d336735417bffe8f16d9bd5a7b3b
+    proxy: 'http://localhost:81/',
+
   });
   gulp.watch('./sass/**/*.scss', ['sass', reload]);
   gulp.watch('./js/*.js', ['js', reload]);
