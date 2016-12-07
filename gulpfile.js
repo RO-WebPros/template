860dcdf0391bd111b053de362e2518165c437d5f
Dragos Vlad <dragos.vlad95@gmail.com>
2016-12-07 19:11:30 +0200

    Added instructions for code standards

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var plumber       = require('gulp-plumber');
 var gutil         = require('gulp-util');
 // var concat        = require('gulp-concat');
 // var uglify        = require('gulp-uglify');
-var jshint        = require('gulp-jshint');
+// var jshint        = require('gulp-jshint');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
@@ -29,10 +29,10 @@ gulp.task('sass', function() {
 
 gulp.task('js', function() {
   return gulp.src(['./js/*.js'])
-  .pipe(jshint())
+ /*   .pipe(jshint())
     .pipe(jshint.reporter('default'))
-//     .pipe(concat('script.js'))
-//     .pipe(uglify())
+    .pipe(concat('script.js'))
+    .pipe(uglify())*/
     .pipe(gulp.dest('./js'))
 });
 
