6f6c75ae1c6e201dc03301f2bb3174466df54924
Daniel <daniel.valcea@gmail.com>
2016-12-12 23:44:41 +0200

    Finished countdown and subscribe without style

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ jQuery(document).ready(function($) {
 	   theme: "rounded-dots-dark"
 	});
 
+ // Countdown timmer
+  $('.counter').countdown('2016/12/31', function(event) {
+        var offset = event.offset;
+        $('#cday').text(offset.totalDays);
+        $('#chours').text(''.concat(offset.hours < 10 ? '0' : '', offset.hours));
+        $('#cminutes').text(''.concat(offset.minutes < 10 ? '0' : '', offset.minutes));
+        $('#cseconds').text(''.concat(offset.seconds < 10 ? '0' : '', offset.seconds));
+    });
 
 	
 });
+
+
